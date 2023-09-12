3dcb0101984b8c41b82c65cdcdf2645d17b36a3e
Egor Pushka <pushkartegor@gmail.com>
2023-09-12 21:34:06 +0300

    add Services layer

---
+++ src/main/java/web/services/CarServiceImpl.java
@@ -0,0 +1,35 @@
+package web.services;
+
+import org.springframework.stereotype.Service;
+import web.dao.CarDao;
+
+import web.model.Car;
+
+import java.util.List;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    private final CarDao carDao;
+
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    @Override
+    public List<Car> indexCarsByCount(int count) {
+
+        if (count == 0) {
+            return carDao.indexCars();
+        } else {
+            return carDao
+                    .indexCars()
+                    .stream()
+                    .limit(Math.abs(count))
+                    .collect(Collectors.toList());
+        }
+    }
+}
+
